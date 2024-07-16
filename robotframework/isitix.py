aabe0f6e6235ab5af3eeb2397df401d465232b02
Mikael Dautrey <mikael.dautrey@isitix.com>
2024-07-16 07:55:58 +0200

    robot test

--- robotframework/isitix.py
+++ robotframework/isitix.py
@@ -1,11 +1,12 @@
-# This program do something
-
 from jimutmap import api
 
 def test_KW(value):
     API = api(value,10,80,10,80)
     print ('Test KW')
     print (API.access_key)
+def get_images():
 
-
-
+# creer un tableau
+# for a in tableau:
+    download a
+# for a in tableau, wait for downloaded
